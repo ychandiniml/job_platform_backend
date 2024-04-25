95565107c8e2cbbb055d9b0dad2332231c66858c
Chandini Mahalakshmi <104729699+ychandiniml@users.noreply.github.com>
2024-04-26 00:00:10 +0530

    created login and logout with database

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,7 @@
 import { catchAsyncErrors } from "../middlewares/catchAsyncError.js";
 import { User } from "../models/userSchema.js";
 import ErrorHandler from "../middlewares/error.js";
+import { sendToken } from "../utils/jwtToken.js";
 
 export const register = catchAsyncErrors(async (req, res, next) => {
   const { name, email, phone, password, role } = req.body;
@@ -18,9 +19,39 @@ export const register = catchAsyncErrors(async (req, res, next) => {
     password,
     role,
   });
-  res.status(200).json({
-    success: true,
-    message: "User Registered",
-    user,
-  });
+  sendToken(user, 201, res, "User Registered!");
+});
+
+export const login = catchAsyncErrors(async (req, res, next) => {
+    const { email, password, role } = req.body;
+    if (!email || !password || !role) {
+      return next(new ErrorHandler("Please provide email ,password and role."));
+    }
+    const user = await User.findOne({ email }).select("+password");
+    if (!user) {
+      return next(new ErrorHandler("Invalid Email Or Password.", 400));
+    }
+    const isPasswordMatched = await user.comparePassword(password);
+    if (!isPasswordMatched) {
+      return next(new ErrorHandler("Invalid Email Or Password.", 400));
+    }
+    if (user.role !== role) {
+      return next(
+        new ErrorHandler(`User with provided email and ${role} not found!`, 404)
+      );
+    }
+    sendToken(user, 201, res, "User Logged In!");
+});
+
+export const logout = catchAsyncErrors(async (req, res, next) => {
+    res
+      .status(201)
+      .cookie("token", "", {
+        httpOnly: true,
+        expires: new Date(Date.now()),
+      })
+      .json({
+        success: true,
+        message: "Logged Out Successfully.",
+      });
 });
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,10 +1,14 @@
 import express from "express";
-import {register} from "../controllers/userController.js";
+import {login, register, logout} from "../controllers/userController.js";
+import { isAuthenticated } from "../middlewares/auth.js";
 
 
 const router = express.Router();
 
 router.post("/register", register);
+router.post("/login", login);
+router.get("/logout", isAuthenticated, logout);
+
 
 
 export default router;
