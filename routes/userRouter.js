8964a943464b18184308de0ec63d3eb81271870f
Chandini Mahalakshmi <104729699+ychandiniml@users.noreply.github.com>
2024-04-26 18:24:06 +0530

    application routes completed

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {login, register, logout} from "../controllers/userController.js";
+import {login, register, logout, getUser} from "../controllers/userController.js";
 import { isAuthenticated } from "../middlewares/auth.js";
 
 
@@ -8,6 +8,7 @@ const router = express.Router();
 router.post("/register", register);
 router.post("/login", login);
 router.get("/logout", isAuthenticated, logout);
+router.get("/getuser", isAuthenticated, getUser);
 
 
 
